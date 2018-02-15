3be07e8c175d43d23e6c60d5e965260e9ae02bde
Fpg13 <fernando.perez.gomez@stud.ki.se>
2018-02-15 11:58:35 +0100

    Add existing file

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir MTLSProject
+cd MTLSProject
+
+mkdir Projects
+touch Projects/readme.txt
+touch Projects/commands.txt
+
+mkdir Datasets
+#touch Datasets/namefile    #touch creates a file inside a directory
+
+mkdir Results
+mkdir Scripts
+mkdir bin
